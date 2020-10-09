8967e376c3b1b504a8d655d5176a23c27ef3417a
WSW <2544894086@qq.com>
2020-10-09 23:16:39 +0800

    first commit

---
+++ cloud-provider-payment8001/src/main/java/com/wsw/cloudproviderpayment8001/CloudProviderPayment8001Application.java
@@ -0,0 +1,13 @@
+package com.wsw.cloudproviderpayment8001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudProviderPayment8001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudProviderPayment8001Application.class, args);
+    }
+
+}
