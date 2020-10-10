7d11557efbadebd4be500e40d4aec7c17ec0cb4e
WSW <2544894086@qq.com>
2020-10-10 23:52:16 +0800

    first commit

---
+++ cloud-consumer-order8000/src/main/java/com/wsw/cloudconsumerorder8000/CloudConsumerOrder8000Application.java
@@ -0,0 +1,13 @@
+package com.wsw.cloudconsumerorder8000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConsumerOrder8000Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConsumerOrder8000Application.class, args);
+    }
+
+}
