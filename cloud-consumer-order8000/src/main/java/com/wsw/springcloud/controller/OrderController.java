5e48a7be97fa83353887eed7ed0eb20c8b476de4
WSW <2544894086@qq.com>
2020-10-12 22:52:48 +0800

    2020-10-12 update

--- cloud-consumer-order8000/src/main/java/com/wsw/springcloud/controller/OrderController.java
+++ cloud-consumer-order8000/src/main/java/com/wsw/springcloud/controller/OrderController.java
@@ -17,7 +17,7 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 @Slf4j
 public class OrderController {
-    private static final String PAYMENT_URL = "http://localhost:8001";
+    private static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVER";
 
     @Autowired
     private RestTemplate restTemplate;
