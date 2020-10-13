993aa4d49afbd680d09bd431190b1f376ecd92e4
wangsongwen <2544894086wsw@gmail.com>
2020-10-13 15:10:02 +0800

    服务提供者集群设置

--- cloud-provider-payment8002/src/main/java/com/wsw/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/wsw/springcloud/controller/PaymentController.java
@@ -5,6 +5,7 @@ import com.wsw.springcloud.entities.Payment;
 import com.wsw.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +23,9 @@ public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @Autowired
     private DiscoveryClient discoveryClient;
 
@@ -31,7 +35,7 @@ public class PaymentController {
         log.info("插入结果: " + result);
 
         if (result > 0){
-            return new CommonResult(200, "插入数据库成功", result);
+            return new CommonResult(200, "插入数据库成功, serverPort: " + serverPort, result);
         }else {
             return new CommonResult(444, "插入数据库失败", null);
         }
@@ -43,7 +47,7 @@ public class PaymentController {
         log.info("查询结果: " + payment);
 
         if (null != payment){
-            return new CommonResult(200, "查询数据成功", payment);
+            return new CommonResult(200, "查询数据成功, serverPort: " + serverPort, payment);
         }else {
             return new CommonResult(444, "查询数据失败, id = " + id, null);
         }
