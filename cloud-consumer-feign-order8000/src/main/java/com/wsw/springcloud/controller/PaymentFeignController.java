132f8173c9ce5d6188927274bdd9615512aa1c22
wangsongwen <2544894086wsw@gmail.com>
2020-10-13 17:11:25 +0800

    2020-10-13 update

---
+++ cloud-consumer-feign-order8000/src/main/java/com/wsw/springcloud/controller/PaymentFeignController.java
@@ -0,0 +1,6 @@
+package com.wsw.springcloud.controller;/**
+* @Author WangSongWen
+* @Date: Created in 17:01 2020/10/13
+* @Description: 
+*/public class PaymentFeignController {
+}
---
+++ cloud-consumer-feign-order8000/src/main/java/com/wsw/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,6 @@
+package com.wsw.springcloud.service;/**
+* @Author WangSongWen
+* @Date: Created in 16:58 2020/10/13
+* @Description: 
+*/public interface PaymentFeignService {
+}
