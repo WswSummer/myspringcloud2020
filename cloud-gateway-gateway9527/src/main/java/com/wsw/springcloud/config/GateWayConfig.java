96922843ee9e453a306efc99b48f2600abae4b78
wangsongwen <2544894086wsw@gmail.com>
2020-10-19 14:04:42 +0800

    Gateway Config

--- cloud-gateway-gateway9527/src/main/java/com/wsw/springcloud/config/GateWayConfig.java
+++ cloud-gateway-gateway9527/src/main/java/com/wsw/springcloud/config/GateWayConfig.java
@@ -1,6 +1,21 @@
-package com.wsw.springcloud.config;/**
-* @Author WangSongWen
-* @Date: Created in 13:54 2020/10/19
-* @Description: 
-*/public class GateWayConfig {
+package com.wsw.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author WangSongWen
+ * @Date: Created in 13:54 2020/10/19
+ * @Description:
+ */
+@Configuration
+public class GateWayConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
+        RouteLocatorBuilder.Builder routes = builder.routes();
+        routes.route("payment_routh_baidu", r -> r.path("/guonei").uri("http://news.baidu.com/guonei")).build();
+        return routes.build();
+    }
 }
