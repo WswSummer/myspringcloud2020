279c2963e67f2fa650e0d9e138c524fb70b470f5
wangsongwen <2544894086wsw@gmail.com>
2020-10-19 15:01:21 +0800

    Gateway 断言

---
+++ cloud-gateway-gateway9527/src/test/java/com/wsw/springcloud/myTest.java
@@ -0,0 +1,15 @@
+package com.wsw.springcloud;
+
+import java.time.ZonedDateTime;
+
+/**
+ * @Author WangSongWen
+ * @Date: Created in 14:43 2020/10/19
+ * @Description:
+ */
+public class myTest {
+    public static void main(String[] args) {
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+    }
+}
