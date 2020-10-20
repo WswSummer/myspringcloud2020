b7ecc8e7b2c5cb5bdfdd56a581b9b25f998e4162
wangsongwen <2544894086wsw@gmail.com>
2020-10-20 15:29:06 +0800

    springcloud config client

--- cloud-config-client3355/src/main/java/com/wsw/springcloud/controller/ConfigClientController.java
+++ cloud-config-client3355/src/main/java/com/wsw/springcloud/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.wsw.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description:
  */
 @RestController
+@RefreshScope
 public class ConfigClientController {
     @Value("${config.info}")
     private String configInfo;
