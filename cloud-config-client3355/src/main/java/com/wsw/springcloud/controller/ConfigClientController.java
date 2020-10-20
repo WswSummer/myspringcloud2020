34ef153e6d481a677626564e8b48e1820944aaa6
wangsongwen <2544894086wsw@gmail.com>
2020-10-20 14:42:05 +0800

    springcloud config client

--- cloud-config-client3355/src/main/java/com/wsw/springcloud/controller/ConfigClientController.java
+++ cloud-config-client3355/src/main/java/com/wsw/springcloud/controller/ConfigClientController.java
@@ -1,6 +1,21 @@
-package com.wsw.springcloud.controller;/**
-* @Author WangSongWen
-* @Date: Created in 14:34 2020/10/20
-* @Description: 
-*/public class ConfigClientController {
+package com.wsw.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author WangSongWen
+ * @Date: Created in 14:34 2020/10/20
+ * @Description:
+ */
+@RestController
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/get")
+    public String getConfigInfo(){
+        return configInfo;
+    }
 }
