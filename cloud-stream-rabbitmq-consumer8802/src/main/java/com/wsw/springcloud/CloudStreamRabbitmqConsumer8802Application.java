b824c5c8351d1edb46ccdc2202ade080efd531b8
wangsongwen <2544894086wsw@gmail.com>
2020-10-21 13:49:19 +0800

    SpringCloud Stream 消息驱动

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/wsw/springcloud/CloudStreamRabbitmqConsumer8802Application.java
@@ -0,0 +1,13 @@
+package com.wsw.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStreamRabbitmqConsumer8802Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudStreamRabbitmqConsumer8802Application.class, args);
+	}
+
+}
