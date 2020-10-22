5215ffc936b66db3a6846bc438ca068b6908c936
wangsongwen <2544894086wsw@gmail.com>
2020-10-22 09:46:34 +0800

    2020-10-22 update

---
+++ cloud-alibaba-nacos-consumer9012/src/main/java/com/wsw/springcloud/controller/PaymentController.java
@@ -0,0 +1,6 @@
+package com.wsw.springcloud.controller;/**
+* @Author WangSongWen
+* @Date: Created in 9:39 2020/10/22
+* @Description: 
+*/public class PaymentController {
+}
---
+++ cloud-alibaba-nacos-consumer9012/src/main/java/com/wsw/springcloud/service/PaymentService.java
@@ -0,0 +1,6 @@
+package com.wsw.springcloud.service;/**
+* @Author WangSongWen
+* @Date: Created in 9:37 2020/10/22
+* @Description: 
+*/public interface PaymentService {
+}
