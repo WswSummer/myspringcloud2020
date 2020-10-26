2b7ba05b51149b8f6c8f4f703e86bdd5018a37c4
wangsongwen <2544894086wsw@gmail.com>
2020-10-26 11:13:55 +0800

    sentinel 热点规则

--- cloud-alibaba-sentinel-service8401/src/main/java/com/wsw/springcloud/controller/SentinelController.java
+++ cloud-alibaba-sentinel-service8401/src/main/java/com/wsw/springcloud/controller/SentinelController.java
@@ -1,7 +1,10 @@
 package com.wsw.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -34,5 +37,16 @@ public class SentinelController {
         int i = 1 / 0;
         log.info("---testC---");
         return "---testC---";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "deal_testHotKey")  // 热点规则
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p1", required = false)String p2){
+        return "---testHotKey---";
+    }
+
+    public String deal_testHotKey(String p1, String p2, BlockException exception){
+        return "---deal_testHotKey---";
     }
 }
