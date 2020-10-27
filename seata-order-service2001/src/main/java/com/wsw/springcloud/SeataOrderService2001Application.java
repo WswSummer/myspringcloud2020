7daaec319f5aa55d3a935f5c6dfb8c890b3aeda3
wangsongwen <2544894086wsw@gmail.com>
2020-10-27 17:48:17 +0800

    2020-10-27 update

---
+++ seata-order-service2001/src/main/java/com/wsw/springcloud/SeataOrderService2001Application.java
@@ -0,0 +1,13 @@
+package com.wsw.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeataOrderService2001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeataOrderService2001Application.class, args);
+	}
+
+}
